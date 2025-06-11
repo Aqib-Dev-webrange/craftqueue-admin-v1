f95c156602386d748514129c6f70803b3a756929
Aqib-Dev-webrange <aqib.ahmed@webrangesolutions.com>
2025-06-11 13:26:54 +0500

    fix the changes

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { ICONS } from "@/constants/icon";
+import Image from "next/image";
 import React, { useState, useEffect, useRef } from "react";
 
 interface SearchInputProps {
@@ -165,14 +167,13 @@ export default function SearchInput({
         {/* Search Icon */}
         {showSearchIcon && (
           <div className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 ${iconSizes[size]}`}>
-            <svg fill="none" stroke="currentColor" viewBox="0 0 24 24">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
+            <Image 
+              src= {ICONS.search}
+              alt="Search Icon"
+              width={20}
+              height={20}
+              className="w-full h-full"
+            />
           </div>
         )}
 
